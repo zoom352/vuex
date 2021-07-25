cc5b77f1035932b607997160d5c0ada381a3f31c
dimakul.com <d-sasha.97@mail.ru>
2021-07-25 16:47:18 +0300

    lesson

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <PostForm/>
+    <p>{{postsCount}}</p>
+    <div class="post" v-for="post in validPosts" :key="post.id">
+      <h2>{{post.title}}</h2>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+import {mapActions} from 'vuex';
+import PostForm from'./components/PostForm.vue'
+
+export default {
+  name: "App",
+  // computed: {
+  //   allPosts() {
+  //     return this.$store.getters.allPosts;
+  //   }
+  // },
+  computed: mapGetters(["allPosts", "postsCount", "validPosts"]),
+  methods: mapActions(['fetchPosts']),
+  components: {PostForm},
+  async mounted() {
+    // this.$store.dispatch('fetchPosts');
+    this.fetchPosts(4);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="submit">
+        <input type="text" v-model="title"/>
+        <input type="text" v-model="body"/>
+        <button type="submit">create post</button>
+    </form>
+</template>
+
+<script>
+import {mapMutations} from "vuex";
+export default {
+    data() {
+        return {
+            title: "",
+            body: ""
+        };
+    },
+    methods: {
+        ...mapMutations(["createPost"]),
+        submit() {
+            this.createPost({
+                title: this.title,
+                body: this.body,
+                id: Date.now()
+            })
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+input {
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+</style>
